9c4236406be1fdc6934b1c273eb6240cb13ce976
ivangonzalez224 <ivan.gonzalezr224@gmail.com>
2023-07-12 11:18:59 -0500

    Add modules and create task function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import _ from 'lodash';
 import './style.css';
+import createTask from './modules/createTask.js';
+import { mainTitle } from './modules/elements.js';
 
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+mainTitle.innerHTML = _.join(["Today's", 'to do'], ' ');
+// crea tasks list//
+createTask();
---
+++ src/modules/elements.js
@@ -0,0 +1,3 @@
+const listPlaceholder = document.querySelector('.list_placeholder');
+export const mainTitle = document.getElementById('main_title');
+export default listPlaceholder;
